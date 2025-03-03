cfb75cede30714487fe0004f252600908f52e34a
fucdan03 <fucdan03@gasos-ro.cz>
2025-03-03 13:10:34 +0100

    autoshop

--- src/elektroshop/Fridge.java
+++ src/elektroshop/Fridge.java
@@ -2,4 +2,57 @@ package elektroshop;
 
 public class Fridge {
 
+    private Energie miraSpotrebi;
+    private String znacka;
+    private String model;
+    private int rokVyroby;
+
+    public Fridge(Energie miraSpotrebi, String znacka, String model, int rokVyroby) {
+        this.miraSpotrebi = miraSpotrebi;
+        this.znacka = znacka;
+        this.model = model;
+        this.rokVyroby = rokVyroby;
+    }
+
+    public void printInfo(){
+        if (miraSpotrebi == Energie.A){
+            System.out.println(rokVyroby+": Very Low");
+        }
+        else if (miraSpotrebi == Energie.B){
+            System.out.println(rokVyroby+": Low");
+        }
+        else if (miraSpotrebi == Energie.C){
+            System.out.println(rokVyroby+": Normal");
+        }
+        else if (miraSpotrebi == Energie.D){
+            System.out.println(rokVyroby+": Above Normal");
+        }
+        else if (miraSpotrebi == Energie.E){
+            System.out.println(rokVyroby+": High");
+        }
+        else if (miraSpotrebi == Energie.F){
+            System.out.println(rokVyroby+": Very High");
+        }
+        else if (miraSpotrebi == Energie.G){
+            System.out.println(rokVyroby+": Extremely High");
+        }
+
+
+    }
+
+    public Energie getMiraSpotrebi() {
+        return miraSpotrebi;
+    }
+
+    public int getRokVyroby() {
+        return rokVyroby;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getZnacka() {
+        return znacka;
+    }
 }
--- src/elektroshop/Items.java
+++ src/elektroshop/Items.java
@@ -1,20 +1,47 @@
 package elektroshop;
-
+import java.util.Scanner;
 public class Items {
     private Fridge[] items;
+    Scanner sc = new Scanner(System.in);
 
-
-    /**
-     * Metoda se zepta uzivatele na informace k pozadovanemu poctu ledniček. A ty prida do pole items.
-     * @param count - pozadovany pocet lednicek
-     */
     public void addItems(int count) {
-      //  items = new ....
-        for (int i=0; i<count; i++){
+        System.out.println("Kolik chces lednicek?");
+        count = sc.nextInt();
+        items = new Fridge[count];
+        String o;
+        for (int i=0; i<count; i++) {
             //nacti od uzivatele míru spotreby a rok výroby ledničky
             //pridej lednicku do pole lednicek
-        }
+            System.out.print("Míra spotřeby: (A-G)");
+            o = sc.nextLine();
+            Energie energie = null;
+            if (o.equals("A")) {
+                energie = Energie.A;
+            } else if (o.equals("B")) {
+                 energie = Energie.B;
+            } else if (o.equals("C")) {
+                 energie = Energie.C;
+            } else if (o.equals("D")) {
+                 energie = Energie.D;
+            } else if (o.equals("E")) {
+                 energie = Energie.E;
+            } else if (o.equals("F")) {
+                 energie = Energie.F;
+            } else if (o.equals("G")) {
+                 energie = Energie.G;
+            }
 
+            System.out.println("Jaký je rok výroby ledničky?");
+            int rokVyroby = sc.nextInt();
+            sc.nextLine();
+            System.out.println("Jaká je značka?");
+            String znacka = sc.nextLine();
+            System.out.println("Jaký je model?");
+            String model = sc.nextLine();
+            
+            
+            items[i] = new Fridge(energie ,znacka, model, rokVyroby);
+        }
     }
 
 
@@ -25,7 +52,9 @@ public class Items {
      */
     public void printInfo(){
         System.out.println("-----INFO O LEDNICKACH-----");
-        //sem dopln kod
-        System.out.println("-----");
+        for (int i=0; i<items.length; i++) {
+            System.out.println(items[i].getRokVyroby()+": "+ items[i].getMiraSpotrebi());
+        }
+        System.out.println("------");
     }
 }
